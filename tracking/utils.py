4481fc2f02828b47cc2dd8671d03d9610c9f98e1
Walther Lee <walthere.lee@gmail.com>
2017-12-24 13:00:40 -0800

    util to export to es added

--- tracking/utils.py
+++ tracking/utils.py
@@ -1,4 +1,7 @@
 import re
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+from .models import Report
 
 
 def get_clean_title(title):
@@ -12,3 +15,28 @@ def get_clean_title(title):
     clean_title = re.sub(r'[^\w\d\s]', '', clean_title)
 
     return clean_title.strip()
+
+
+def export_reports_to_es():
+    es = Elasticsearch("http://45.77.161.88:9200")
+
+    actions = []
+    for report in Report.objects.all().iterator():
+        action = {
+            "_op_type": "create",
+            "_index": "ppg-mml",
+            "_type": "report",
+            "_id": str(report.id),
+            "ml-id": report.ml_id,
+            "title": report.title,
+            "first_date": report.first_date,
+            "last_date": report.last_date,
+            "last_price": report.last_price,
+            "is_new": report.is_new,
+            "free_shipping": report.free_shipping,
+            "accepts_mercadopago": report.accepts_mercadopago,
+            "sold_quantity": report.sold_quantity,
+            "address": report.address
+        }
+        actions.append(action)
+    bulk(es, actions)
